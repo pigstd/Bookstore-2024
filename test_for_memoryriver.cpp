bb41a697767b2ccf352c4e0787b7959c07783913
pigstd <zhuzemu@sjtu.edu.cn>
2024-12-17 20:52:21 +0800

    文件读写操作

--- test_for_memoryriver.cpp
+++ test_for_memoryriver.cpp
@@ -1,4 +1,4 @@
-#include "MemoryRiver.h"
+#include "MemoryRiver.hpp"
 #include<bits/stdc++.h>
 
 using std::cout;
@@ -7,11 +7,21 @@ int main()
 {
     MemoryRiver<int,10> file_1;
     file_1.initialise("file1");
-    int a = 233, b;
+    for (int i = 1; i <= 10; i++)
+        file_1.write_info(i, i);
+    for (int i = 10; i >= 1; i--)
+    {
+        int k;
+        file_1.get_info(k, i);
+        cout << k << ' ';
+    }
+    int a = 233, b, c;
     cout << (b = file_1.write(a)) << '\n';
     a = 344;
-    cout << file_1.write(a) << '\n';
-    file_1.read(a,b);
+    cout <<  (c = file_1.write(a)) << '\n';
+    file_1.read(a, b);
     cout << a << '\n';
+    file_1.read(b, c);
+    cout << b << '\n';
     return 0;
 }
